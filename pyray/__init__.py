d6fd92a36d0e438a0aab9c3250e946a0a04d9b5f
patrickW <patrick.wakeem@apicasystem.com>
2016-03-12 09:04:05 -0800

    +version

--- pyray/__init__.py
+++ pyray/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__author__ = 'Matt Welch'
-__email__ = 'mwelch@tallshorts.com'
-__version__ = '0.1.0'
+__author__ = 'Matt Welch, Patrick W.'
+__email__ = 'mwelch@tallshorts.com, patwakeem@gmail.com'
+__version__ = '2016.3.12'
